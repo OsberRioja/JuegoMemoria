52bb26f7d944c5d3d047cbf41602c06c51ba054b
Osber <osberriojalopez@gmail.com>
2023-11-22 17:58:04 -0400

    RamaPropositions: se puede emparejar numeros con letras

--- main.js
+++ main.js
@@ -11,46 +11,41 @@ let timer=false;
 let seconds=60;
 let countdown=null;
 let timerLetter=document.getElementById('tiempo');
+let firstId=null;
+let secondId=null;
 
-let propositions=['a','b','c','d','e','f','g','h'];
-let equivalence=['1','2','3','4','5','6','7','8'];
+let cards = [
+    { value: 'a'},
+    { value: 'b'},
+    { value: 'c'},
+    { value: 'd'},
+    { value: 'e'},
+    { value: 'f'},
+    { value: 'g'},
+    { value: 'h'},
+    { value: '1'},
+    { value: '2'},
+    { value: '3'},
+    { value: '4'},
+    { value: '5'},
+    { value: '6'},
+    { value: '7'},
+    { value: '8'},
+];
 
-// Función para crear pares de elementos
-function createPairs(arr1, arr2) {
-    let pairs = [];
-    for (let i = 0; i < arr1.length; i++) {
-        pairs.push([arr1[i], arr2[i]]);
-    }
-    return pairs;
-}
-//crear pares de proposiciones y equivalencias
-let pairs = createPairs(propositions, equivalence);
-
-// Función para desordenar manteniendo la relación entre los pares
-function shufflePairs(pairs) {
-    let currentIndex = pairs.length, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // Intercambiar elementos mientras se mantiene la relación de pares
-        [pairs[currentIndex], pairs[randomIndex]] = [pairs[randomIndex], pairs[currentIndex]];
-    }
-    return pairs;
+function shuffleCards(cards) {
+    cards = cards.sort(()=>{return Math.random()-0.5});
 }
 
-// Desordenar los pares manteniendo la relación
-pairs = shufflePairs(pairs);
+//establecer pares
+let pairs = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8,
+     '1':'a','2':'b','3':'c','4':'d','5':'e','6':'f','7':'g','8':'h'
+};
 
-// Separar los pares desordenados en sus arreglos respectivos
-for (let i = 0; i < pairs.length; i++) {
-    propositions[i] = pairs[i][0];
-    equivalence[i] = pairs[i][1];
-}
-console.log(propositions);
-console.log(equivalence);
-
+// Desordenar las cartas
+shuffleCards(cards);
+console.log(cards);
 
 
 function countTime()
@@ -71,7 +66,7 @@ function blockCards()
     for(var i=0;i<=15;i++)
     {
         let blockedCard=document.getElementById(i);
-        blockedCard.innerHTML = propositions[i];
+        blockedCard.innerHTML = cards[i].value;
         blockedCard.disabled=true;
     }
 }
@@ -86,17 +81,16 @@ function uncover(id)
     uncoveredCards++;
     if(uncoveredCards==1){
         card1=document.getElementById(id);
-        firstResult=propositions[id];
+        firstResult=cards[id].value;
         card1.innerHTML = firstResult;
-
         card1.disabled=true;
     }else if(uncoveredCards==2){
         card2=document.getElementById(id);
-        secondResult = propositions[id];
+        secondResult = cards[id].value;
         card2.innerHTML = secondResult;
         card2.disabled=true;
         IncreaseMovement();
-        if(firstResult==secondResult)
+        if(pairs[secondResult]==firstResult)
         {
             uncoveredCards=0;
             IncreaseSuccess();
